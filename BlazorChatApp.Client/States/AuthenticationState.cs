2db9485b0e8ca87c1f39cf92a1571647bab39896
kevdev <holer.kevin@gmail.com>
2026-05-08 16:01:18 +0200

    add authentication state

---
+++ BlazorChatApp.Client/States/AuthenticationState.cs
@@ -0,0 +1,40 @@
+﻿using BlazorChatApp.Shared.DTO;
+using System.ComponentModel;
+
+namespace BlazorChatApp.Client.States
+{
+    public class AuthenticationState: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public string? Name { get; set; }
+        public string? Token { get; set; }
+
+        private bool _isAuthenticated;
+
+        public bool IsAuthenticated
+        {
+            get =>_isAuthenticated;
+            set { 
+                if (_isAuthenticated != value)
+                {
+                    _isAuthenticated = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsAuthenticated)));
+                }
+                
+            }
+        }
+        public void LoadState(AuthResponseDto authResponseDto)
+        {
+            Name = authResponseDto.Name;
+            Token = authResponseDto.Token;
+           IsAuthenticated = true;
+
+        }
+        public void UnLoadState(AuthResponseDto authResponseDto)
+        {
+            Name = null;
+            Token = null;
+            IsAuthenticated = false;
+        }
+    }
+}
